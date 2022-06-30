b953e1100ddd276af594ab5ab6a721436b51f0fa
elishaishapiro <elishaishapiro519@gmail.com>
2022-06-29 23:18:32 -0700

    fix qr error

--- src/qr-viewer.js
+++ src/qr-viewer.js
@@ -5,6 +5,7 @@ window.addEventListener("load", () => {
   const reset = document.getElementById("reset");
 
   const text = `${data.scout}\t${data.teamNumber}\t${data.matchNumber}\t${data.startPosition}\t${data.taxi}\t${data.shotsMadeHighAuto}\t${data.shotsMadeLowAuto}\t${data.shotsMissAuto}\t${data.shotsMadeHighTele}\t${data.shotsMadeLowTele}\t${data.shotsMissTele}\t${data.startClimb}\t${data.rung}\t${data.failClimb}\t${data.wasDefended}\t${data.playedDefense}\t${data.gotDisabled}\t${data.notes}`;
+  text = text.padEnd(220);
 
   let qrcode = undefined;
 
